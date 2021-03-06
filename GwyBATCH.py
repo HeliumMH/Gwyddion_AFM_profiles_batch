53cc5ea1f3d1f31f95a1187130dbe8086418b8e0
HeliumMH <66976281+HeliumMH@users.noreply.github.com>
2021-03-06 15:27:59 +0000

    Update GwyBATCH.py

--- GwyBATCH.py
+++ GwyBATCH.py
@@ -7,6 +7,7 @@ __author__='HeliumMH'
 # pure comma separated file as input format
 intro='---Gwy AFM line profiles batch processing program--- \nPlease enter the dir of the csv:\n'
 file_dir=input(intro)
+output_path=os.path.split(file_dir)[0]
 
 while file_dir != []:
     if file_dir=='quit':
@@ -21,7 +22,8 @@ while file_dir != []:
         df=pd.DataFrame(data)
         nline = df.shape[1]
 
-        with open('AFM thickness merge.txt', 'a+') as f:
+        output_path=output_path+'\AFM thickness merge.txt'
+        with open(output_path, 'a+') as f:
             print('--- %s write at' %file_dir,datetime.now(),' ---',file=f)
         n = 1
         nProfile = 1
@@ -31,7 +33,7 @@ while file_dir != []:
             y1=df.iloc[df.count()[n]-1,n] #use the last non 'NaN' value
             height=abs(y0-y1)*1000000000 #use 'nm' as unit
             print(height)
-            with open('AFM thickness merge.txt','a+') as f:
+            with open(output_path,'a+') as f:
                 print(nProfile,'%.2f' %height,sep='   ',file=f)
             height=0
             n+=2
