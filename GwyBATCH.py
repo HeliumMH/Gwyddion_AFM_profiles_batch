1af04bb621a0b7e05aa93a06ccfc4c93d26055ca
HeliumMH <66976281+HeliumMH@users.noreply.github.com>
2021-03-06 14:42:38 +0000

    Update GwyBATCH.py

--- GwyBATCH.py
+++ GwyBATCH.py
@@ -2,32 +2,34 @@ import pandas as pd
 import os
 from datetime import datetime
 
-file_dir=input('---Gwy AFM line profiles batch processing program--- \nPlease enter the dir of the csv file\n')
+__author__='HeliumMH'
+
+# pure comma separated file as input format
+intro='---Gwy AFM line profiles batch processing program--- \nPlease enter the dir of the csv:\n'
+file_dir=input(intro)
 
 while file_dir != []:
     if file_dir=='quit':
         quit()
     elif os.path.exists(file_dir)==False:
         print('File not exists or No directory detected')
-        file_dir = input('---Gwy AFM line profiles batch processing program--- \nPlease enter the dir of the csv file\n')
+        file_dir = input(intro)
         continue
     else:
         #read data
         data = pd.read_csv(file_dir,sep=';',header=None)
         df=pd.DataFrame(data)
-        #print(df)
-        #print(df.count())
         nline = df.shape[1]
+
         with open('AFM thickness merge.txt', 'a+') as f:
             print('--- %s write at' %file_dir,datetime.now(),' ---',file=f)
-        #processing
         n = 1
         nProfile = 1
+
         while n<nline-1:
             y0=df.iloc[0,n]
-            y1=df.iloc[df.count()[n]-1,n]
-            #print(y0,y1)
-            height=abs(y0-y1)*1000000000
+            y1=df.iloc[df.count()[n]-1,n] #use the last non 'NaN' value
+            height=abs(y0-y1)*1000000000 #use 'nm' as unit
             print(height)
             with open('AFM thickness merge.txt','a+') as f:
                 print(nProfile,'%.2f' %height,sep='   ',file=f)
@@ -35,9 +37,6 @@ while file_dir != []:
             n+=2
             nProfile+=1
 
-        file_dir = input('Gwy AFM line profiles batch processing program. Please enter the dir of the csv file\n')
+        file_dir = input(intro)
         continue
-
-
-
 
